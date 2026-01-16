8792ca36d2fa2064725a413f7e898b06d1083f0e
Kietbeve <nguyentuankiet0618@gmail.com>
2026-01-16 12:53:32 +0700

    fix: sua loi xem my-account khi chua dang nhap

--- frontend-bepviet4.0/src/view/pages/user/UserProfile.jsx
+++ frontend-bepviet4.0/src/view/pages/user/UserProfile.jsx
@@ -22,16 +22,17 @@ export default function UserProfile() {
 
   const [blogs,setBlogs]=useState([])
   useEffect(() => {
-  if (!username && myAccount){
+  if (!username){
+    if(!myAccount){
+      navigate("/login")
+    }
     setUser_Info(myAccount)
     setCookbooks(myAccount.cookbooks)
     setRecipes(myAccount.posts.filter(p=>p.type=="Công thức"))
     setBlogs(myAccount.posts.filter(p=>p.type=="Blog"))
     return
   }
-  else{
-    navigate("login")
-  };
+ ;
 
   const fetchUser = async () => {
     try {
